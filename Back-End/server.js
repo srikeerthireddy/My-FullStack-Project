14b0fb1627cca2f92e46b45ba6cf243efac11921
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-04-05 13:43:29 +0530

    Simple Api page

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,9 +2,9 @@ const express=require('express');
 const app=express();
 const port=4004;
 app.use(express.json());
-// app.get('/',(req,res)=>{
-//     return (res.json({name:req.body.name,squadName:req.body.squadName}));
-// });
+app.post('/',(req,res)=>{
+    return (res.json({name:req.body.name,squadName:req.body.squadName}));
+});
 app.get('/',(req,res)=>{
     res.send("Back End Project");
 })
