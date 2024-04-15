f6f3754a612a987172c9da88af1c183f5f57b6b9
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-04-15 11:30:58 +0530

    Another method of mongodb-connection

--- Back-End/config/db.js
+++ Back-End/config/db.js
@@ -1,13 +1,11 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-const connectDB=mongoose.connect(process.env.MONGODB_URI,{
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-})
-.then(()=>console.log('Connected to MONGODB'))
-.catch(err=>{
-   console.log("Error in the MONGODB",err);
-   process.exit(1);
-})
-
+const connectDB=async()=>{
+   try{
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Mongodb connected");
+   }catch(err){
+       console.log("Error connecting to the mongodb",err);
+   }
+}
 module.exports=connectDB;
